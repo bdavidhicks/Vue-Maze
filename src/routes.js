31919afeab74ee8e10d78c835259cd8d75bec7a3
Brian Hicks <brian.david.hicks@gmail.com>
2021-11-26 02:35:19 -0700

    Added catch all route

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ import Home from './components/Home.vue';
 Vue.use(VueRouter);
 
 export default [
-  { path: '', component: Home },
+  { path: '/', component: Home },
+  { path: '/:catchAll(.*)', redirect: '/' },
 ];
